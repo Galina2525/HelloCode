aaf809b57032454962e7cf0b3dafb9062b3ed802
Galina <koroleva2525.gk@gmail.com>
2022-10-24 12:10:50 +0200

    add Ex005_Conditions_if_else

---
+++ Example005_ConditionItEls/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine(); 
+
+if (username.ToLower() == "galina")
+{
+    Console.WriteLine("Wow! It is Galina!");
+}
+else
+{
+    Console.Write("Welcome ");
+    Console. WriteLine(username);
+    
+}
+
+
+
